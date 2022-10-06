018f92a005959619959dc8ed697c24c31fe07de1
Fowziya30 <115149789+Fowziya30@users.noreply.github.com>
2022-10-06 17:15:19 +0530

    Add files via upload

---
+++ Intergral.java
@@ -0,0 +1,20 @@
+package datatypleExample;
+
+public class Intergral {
+
+	public static void main(String[] args) {
+		byte number=10;
+		short number1=4;
+		int number2=2345;
+		long number3=67893625;
+		float number4=6.7f;
+		double number5=6.77;
+		System.out.println(number);
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(number4);
+		System.out.println(number5);
+	}
+
+}
