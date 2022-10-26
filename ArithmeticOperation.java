9735939a44d73ac8868fd1c5dac904edfd58a387
Fowziya30 <115149789+Fowziya30@users.noreply.github.com>
2022-10-26 14:34:58 +0530

    Add files via upload

---
+++ ArithmeticOperation.java
@@ -0,0 +1,24 @@
+package activity1;
+
+public class ArithmeticOperation {
+
+	public static void main(String[] args) {
+		int x=23;
+		int y=34;
+		int add=x+y;//addition operation
+		int multi=x*y;
+		int div=x/y;
+		int mod=x%y;
+		int sub=x-y;
+		
+		System.out.println("x+y="+add);
+		System.out.println("x*y="+multi);
+		System.out.println("x%y="+div);
+		System.out.println("x%y="+mod);
+		System.out.println("x-y="+sub);
+		
+		
+
+	}
+
+}
